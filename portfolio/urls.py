2738641c96716eba609b8506c85565a3367641fb
unknown <akashbadole@rediffmail.com>
2020-11-29 14:57:46 +0530

    Bootstrap Added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from portfolio import views
 urlpatterns = [
-    path('',views.index, name='home')
+    path('',views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contact',views.contact, name="contact")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is homepage")
+    # context = {
+    #     "va1" : "akash first program"
+    # }
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
+
+def about(request):
+    return HttpResponse("About Page")
+
+def contact(request):
+    return HttpResponse("contact page")
